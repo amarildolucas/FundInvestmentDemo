d41cd9e2bec43e1bdfbc9d4c72de0a0deab3436e
Amarildo Lucas <vmarildo@gmail.com>
2019-04-14 11:15:47 -0300

    added get funds list request

---
+++ FundInvestmentDemo/API/APIClient+Requests.swift
@@ -0,0 +1,24 @@
+//
+//  APIClient+Requests.swift
+//  FundInvestmentDemo
+//
+//  Created by Amarildo Lucas on 14/04/2019.
+//  Copyright © 2019 Amarildo. All rights reserved.
+//
+
+import Alamofire
+import AlamofireObjectMapper
+import Foundation
+
+extension APIClient {
+  static func getFundsList(completion: @escaping ResponseCompletion, errorHandler: @escaping ErrorHandler) {
+    Alamofire.request("\(baseUrl.rawValue)\(Resources.fundDetailFull)", method: .get, headers: nil).responseArray { (response: DataResponse<[FundInvestment]>) in
+      switch response.result {
+      case .success(let value as? FundInvestment):
+        completion(value)
+      case .failure(let error):
+        errorHandler(error, response.response?.statusCode)
+      }
+    }
+  }
+}
