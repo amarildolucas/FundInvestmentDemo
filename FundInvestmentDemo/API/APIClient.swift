54983de93d921bc20e9f0f09b60ef349387d0046
Amarildo Lucas <vmarildo@gmail.com>
2019-04-14 11:07:39 -0300

    added basic api client

---
+++ FundInvestmentDemo/API/APIClient.swift
@@ -0,0 +1,25 @@
+//
+//  APIClient.swift
+//  FundInvestmentDemo
+//
+//  Created by Amarildo Lucas on 14/04/2019.
+//  Copyright © 2019 Amarildo. All rights reserved.
+//
+
+import Foundation
+
+struct APIClient {
+  struct Resources {
+    static let fundDetailFull = "/fund_detail_full.json"
+  }
+  
+  enum APIEnvironment: String {
+    case staging = "https://s3.amazonaws.com/orama-media/json"
+  }
+  
+  static let baseUrl: APIEnvironment = .staging
+
+  typealias ResponseCompletion = (Any) -> Void
+  
+  typealias ErrorHandler = (_ error: Error, _ statusCode: Int?) -> Void
+}
---
+++ Pods/Target Support Files/Pods-FundInvestmentDemo/Pods-FundInvestmentDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FundInvestmentDemo : NSObject
+@end
+@implementation PodsDummy_Pods_FundInvestmentDemo
+@end
